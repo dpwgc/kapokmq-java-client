0076c0eeae836a93b4df29964eda7e7cb533779b
帅逼 <281148314@qq.com>
2022-01-28 02:08:20 +0800

    消息队列节点模板

--- src/main/java/com/dpwgc/kapokmq/model/Node.java
+++ src/main/java/com/dpwgc/kapokmq/model/Node.java
@@ -2,9 +2,9 @@ package com.dpwgc.kapokmq.model;
 
 // Node 消息队列服务节点结构体
 public class Node {
-    private String Name;
-    private String Addr;
-    private String Port;
+    private String Name; //节点名称："mq:127.0.0.1:8011"
+    private String Addr; //节点ip地址
+    private String Port; //节点http服务端口号
 
     public void setName(String name) {
         Name = name;
