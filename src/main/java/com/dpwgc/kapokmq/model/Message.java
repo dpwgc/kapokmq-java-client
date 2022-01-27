8ec7a12e72b18ba43de4380e877b579ab4bcbd03
帅逼 <281148314@qq.com>
2022-01-28 02:08:03 +0800

    消息模板

--- src/main/java/com/dpwgc/kapokmq/model/Message.java
+++ src/main/java/com/dpwgc/kapokmq/model/Message.java
@@ -3,13 +3,13 @@ package com.dpwgc.kapokmq.model;
 // Message 消费者接收的消息模板
 public class Message {
 
-    private String MessageCode;
-    private String MessageData;
-    private String Topic;
-    private long CreateTime;
-    private long DelayTime;
-    private long ConsumedTime;
-    private int Status;
+    private String MessageCode; //消息标识码
+    private String MessageData; //消息主体（一般为json字符串）
+    private String Topic;       //消息所属主题
+    private long CreateTime;    //消息创建时间
+    private long DelayTime;     //消息延时推送的时间（单位：秒）
+    private long ConsumedTime;  //消息确认消费时间
+    private int Status;         //消息状态（-1：未消费，0：消费失败/未消费的延时消息，1：已消费）
 
     public void setDelayTime(long delayTime) {
         DelayTime = delayTime;
--- src/main/java/com/dpwgc/kapokmq/model/SendMessage.java
+++ src/main/java/com/dpwgc/kapokmq/model/SendMessage.java
@@ -3,8 +3,8 @@ package com.dpwgc.kapokmq.model;
 // SendMessage 生产者发送的消息模板
 public class SendMessage {
 
-    private String MessageData;
-    private long DelayTime;
+    private String MessageData; //消息主体
+    private long DelayTime;     //消息延时推送的时间（单位：秒）
 
     public void setMessageData(String messageData) {
         MessageData = messageData;
