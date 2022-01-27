6b65d1d4c2060b001a9896588c78230405600e7f
帅逼 <281148314@qq.com>
2022-01-28 02:09:47 +0800

    json解析工具

--- src/main/java/com/dpwgc/kapokmq/utils/JsonReadUtil.java
+++ src/main/java/com/dpwgc/kapokmq/utils/JsonReadUtil.java
@@ -1,6 +1,8 @@
 package com.dpwgc.kapokmq.utils;
 
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.dpwgc.kapokmq.model.Message;
 import com.dpwgc.kapokmq.model.Node;
 
 import java.util.ArrayList;
@@ -11,10 +13,9 @@ import java.util.List;
  */
 public class JsonReadUtil {
 
-    public static List<Node> getList(String json){
+    public static List<Node> getNodes(String json){
         List<Node> nodes = new ArrayList<>();
         try {
-            //转换为Map对象
             nodes = JSONArray.parseArray(json, Node.class);
         }
         catch (Exception e) {
@@ -23,4 +24,14 @@ public class JsonReadUtil {
         return nodes;
     }
 
+    public static Message getMessage(String json){
+        Message message = new Message();
+        try {
+            message = JSONObject.parseObject(json, Message.class);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
 }
