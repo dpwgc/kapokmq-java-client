ba2fb9b37db5e11c3e4acbaab2b90077622f74a4
帅逼 <281148314@qq.com>
2022-01-28 02:09:30 +0800

    http请求工具

--- src/main/java/com/dpwgc/kapokmq/utils/HttpUtil.java
+++ src/main/java/com/dpwgc/kapokmq/utils/HttpUtil.java
@@ -15,33 +15,38 @@ public class HttpUtil {
     public static String sendPost(String url, String secretKey) {
         PrintWriter out = null;
         BufferedReader in = null;
-        String result = "";
+        StringBuilder result = new StringBuilder();
         try {
             URL realUrl = new URL(url);
+
             // 打开和URL之间的连接
             URLConnection conn = realUrl.openConnection();
-            // 设置通用的请求属性
+
+            // 设置请求头
             conn.setRequestProperty("accept", "*/*");
             conn.setRequestProperty("connection", "Keep-Alive");
             conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
             //设置访问密钥
             conn.setRequestProperty("secretKey", secretKey);
+
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
             conn.setDoInput(true);
+
             // 获取URLConnection对象对应的输出流
             out = new PrintWriter(conn.getOutputStream());
+
             // flush输出流的缓冲
             out.flush();
+
             // 定义BufferedReader输入流来读取URL的响应
             in = new BufferedReader(
                     new InputStreamReader(conn.getInputStream()));
             String line;
             while ((line = in.readLine()) != null) {
-                result += line;
+                result.append(line);
             }
         } catch (Exception e) {
-            System.out.println("发送 POST 请求出现异常！"+e);
             e.printStackTrace();
         }
         //使用finally块来关闭输出流、输入流
@@ -58,7 +63,7 @@ public class HttpUtil {
                 ex.printStackTrace();
             }
         }
-        return result;
+        return result.toString();
     }
 
 }
